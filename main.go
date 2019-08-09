9c9de5da58e979935041e764363c2bf2cc99bbbf
TheSecEng <zachary.schulze@gmail.com>
2019-08-09 10:01:04 -0400

    Updating debugging method

--- main.go
+++ main.go
@@ -13,11 +13,7 @@ func main() {
 
 	client := cloudcheckr.NewEnvClient(nil, "")
 	client.SetDebug(true)
-<<<<<<< HEAD
 	details, err := client.GetEC2AddressDetails(context.Background(), nil)
-=======
-	details, err := client.GetRouteTables(context.Background(), nil)
->>>>>>> cbeade4ab65e7e8f1a4400d399b2b8f653e2f389
 
 	if err != nil {
 		log.Panic(err)
@@ -25,8 +21,5 @@ func main() {
 		data, _ := json.MarshalIndent(&details, "", "  ")
 		fmt.Println(string(data))
 	}
-<<<<<<< HEAD
 
-=======
->>>>>>> cbeade4ab65e7e8f1a4400d399b2b8f653e2f389
 }
