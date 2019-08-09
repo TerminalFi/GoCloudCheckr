28781ec7c302b41c63085148d9bb65a68368c162
TheSecEng <zachary.schulze@gmail.com>
2019-08-09 10:06:04 -0400

    Update main.go example file

--- main.go
+++ main.go
@@ -4,19 +4,24 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"log"
 
 	"github.com/theseceng/gocloudcheckr/cloudcheckr"
 )
 
 func main() {
 
+	// Create a new CloudCheckr Client for making API calls
 	client := cloudcheckr.NewEnvClient(nil, "")
+
+	// Enable debugging to log error messages and debug messages
+	// Should probably seperate the two
 	client.SetDebug(true)
+
+	// Make a API call to get EC2 Address Details
 	details, err := client.GetEC2AddressDetails(context.Background(), nil)
 
 	if err != nil {
-		log.Panic(err)
+		client.Debug("Issue with API call", nil, err)
 	} else {
 		data, _ := json.MarshalIndent(&details, "", "  ")
 		fmt.Println(string(data))
